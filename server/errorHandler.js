09271b44ebc2e94060e13f8d86a493f9a74d3951
Paul <pavelmkdev@gmail.com>
2023-01-05 10:20:47 +0200

    init

---
+++ server/errorHandler.js
@@ -0,0 +1,13 @@
+const { ValidationError } = require('yup');
+const UserError = require('./errors/UserError');
+
+module.exports.errorHandler = async (err, req, res, next) => {
+  if (err instanceof ValidationError) {
+    return res.status(400).send(err.message);
+  }
+  if (err instanceof UserError) {
+    return res.status(400).send(err.message);
+  } else {
+    return res.status(400).send(err.message);
+  }
+};
